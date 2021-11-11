14869af08ec93335dfdeb8b8eb80196eff6c53c0
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-11 17:35:17 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,38 @@
 import socket
-from time import sleep
+import threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+nickname = input("Как Вас зовут?")
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 5555))
 
-data = sock.recv(1024)
 
-sock.close()
+def send():
+    while True:
+        message = f'{nickname}: {input()}'
+        if message.replace(' ','').split(':')[-1] == 'exit':
+            client.close()
+            break
+        else:
+            client.send(message.encode('UTF-8'))
 
-print(data.decode())
+
+def get():
+    while True:
+        try:
+            message = client.recv(1024).decode('UTF-8')
+            if message == 'write_name':
+                client.send(nickname.encode('UTF-8'))
+            else:
+                print(message)
+        except:
+            print("Отключение от сервера")
+            client.close()
+            break
+
+
+receive_thread = threading.Thread(target=get)
+receive_thread.start()
+
+write_thread = threading.Thread(target=send)
+write_thread.start()
