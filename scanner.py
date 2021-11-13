aa65f60ad1c70926c34c0b6d175c45dfc976ed40
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-13 21:09:17 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,15 +1,57 @@
+import threading
 import socket
-from threading import Thread
+from tqdm import tqdm
 
-N = 2**16 - 1
+target = input('Введите ip адрес или 1 для стандартного ip: ')
+if target == '1':
+    target = '127.0.0.1'
 
-for port in range(1,100):
-    sock = socket.socket()
+k_min = int(input('Введите начальный порт для проверки: '))
+while True:
+    if k_min < 0:
+        print('Вы ввели неверный порт')
+        k_min = int(input('Введите начальный порт для проверки: '))
+    else:
+        break
+
+k_max = int(input('Введите конечный порт для проверки: '))
+while True:
+    if k_max >= k_min:
+        break
+    else:
+        print('Вы ввели неверный порт')
+        k_max = int(input('Введите конечный порт для проверки: '))
+
+opened = []
+
+
+def portscan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        con = s.connect((target, port))
+
+        opened.append(port)
+        con.close()
     except:
-        continue
-    finally:
-        sock.close()
+        pass
+
+
+r = 1
+print('--------------ПРОВЕРКА НАЧАТА--------------')
+for x in tqdm(range(k_min, k_max + 1)):
+    t = threading.Thread(target=portscan, kwargs={'port': r})
+
+    r += 1
+    t.start()
+
+print('--------------ПРОВЕРКА ОКОНЧЕНА--------------')
+
+closed = []
+for i in range(k_min, k_max + 1):
+    if i not in opened:
+        closed.append(i)
+print('Открытые порты: ', *opened)
+print('--------------------------------------------')
+print('Закрытые порты: ', *closed)
