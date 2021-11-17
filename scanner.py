2fd4ac161e53f673d5346111674b63df9b7dbc4f
nastyamak <90025016+nastyamak@users.noreply.github.com>
2021-11-17 19:11:37 +0300

    Максимова ПИ20-1

--- scanner.py
+++ scanner.py
@@ -1,15 +1,49 @@
+import threading
 import socket
-from threading import Thread
-
+from tqdm import tqdm
 N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
+adr = '62.117.90.2'
+free_port = []
+start = 0
+finish = 65535
+Numbers_Of_Threads = 3000 # потоки
+one_percent = int((finish - start) / 100)
+all_port = [i for i in range(start, finish)]
+procent = 0
+pbar = tqdm(total=100)
+
+
+def port_connect(adr, port):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    if conn.connect_ex((adr, port)):
         print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+        print()
+        return port
+    else:
+        return False
+
+
+def port_thread():
+    while all_port:
+        global procent
+        global loading
+        port = all_port.pop()
+        procent += 1
+        if procent % one_percent == 0:
+            pbar.update(1)
+        if port_connect(adr, port):
+            free_port.append(port)
+    pbar.close()
+
+
+threads = [threading.Thread(target=port_thread) for i in range(Numbers_Of_Threads)]
+
+[i.start() for i in threads]
+[i.join() for i in threads]
+
+print("\n")
+lenght = len(free_port)
+print(f"Список открытых портов (всего {lenght}):")
+print(sorted(free_port)) # отсортированный список доступных портов
+print(len(free_port)) # количество доступных портов
