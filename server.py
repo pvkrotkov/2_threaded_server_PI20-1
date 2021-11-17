9ca4616e32788c9299ad4054b160f989decbb1fa
anna01122002 <76559002+anna01122002@users.noreply.github.com>
2021-11-17 19:23:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import *
 
 sock = socket.socket()
 sock.bind(('', 9090))
@@ -7,6 +8,7 @@ conn, addr = sock.accept()
 print(addr)
 
 msg = ''
+clients = []
 
 while True:
 	data = conn.recv(1024)
@@ -16,5 +18,29 @@ while True:
 	conn.send(data)
 
 print(msg)
+def listener(conn_m, addr_m):
+    print("Регистрация нового клиента")
+    while True:
+        msg = ''
+        data = conn_m.recv(1024)
+        msg += data.decode()
+        conn_m.send("Вы написали: ".encode() + data)
+        for client_each in clients:
+            if client_each != conn_m:
+                client_each.send(str(addr_m[0]).encode() + ": ".encode() + data)
+        print(addr_m[0] + ": " + msg)
+
 
 conn.close()
+print("Сервер запущен")
+while True:
+    conn, addr = sock.accept()
+    msg = ''
+    data = conn.recv(1024)
+    msg += data.decode()
+    if msg != 'scanner_command':
+        clients.append(conn)
+        client = Thread(target=listener, args=(conn, addr))
+        client.run()
+    else:
+        continue
